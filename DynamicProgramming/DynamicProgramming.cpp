40f2716e5082d68e7ab1e5b122ada3b149ae2b22
Orion Kostival <okostiva@mines.edu>
2012-10-25 21:09:30 -0600

    Converted to recursive

--- DynamicProgramming/DynamicProgramming.cpp
+++ DynamicProgramming/DynamicProgramming.cpp
@@ -4,15 +4,15 @@
 
 using namespace std;
 
-string reverseString(string input);
+void printLongestSubsequenceArray(int**& results, string input);
 string trace(int**& results, string input, int row, int col);
+void calcResultsRow(int**& result, int row, string input);
+void calcResultsCol(int**& result, int row, int col, string input);
 
 int main() 
 {
 	string input;
-	string reverseInput;
 	string longestSubsequence;
-	int stringLen = 0;
 	int** results;
 
 	//A C G T G T C A A A A T C G
@@ -21,41 +21,18 @@ int main()
 	cout << "Please enter a string: " << flush;
 	cin >> input;
 
-	stringLen = input.length();
-	
-	reverseInput = reverseString(input);
+	results = new int* [input.length() + 1];
 
-	results = new int* [stringLen + 1];
-
-	for (int i=0; i<=stringLen; i++)
+	for (int i=0; i<=input.length(); i++)
 	{
-		results[i] = new int [stringLen + 1];
+		results[i] = new int [input.length() + 1];
 	}
 
-	for (int i=0; i<=stringLen; i++)
-	{
-		results[i][0] = 0;
-		results[0][i] = 0;
-	}
+	calcResultsRow(results, 0, input);
+	//printLongestSubsequenceArray(results, input); //Debug output for the results array
+	longestSubsequence = trace(results, input, input.length(), input.length());
 
-	for (int i=1; i<=stringLen; i++)
-	{
-		for (int j=1; j<=stringLen; j++)
-		{
-			if (input[i-1] == reverseInput[j-1])
-			{
-				results[i][j] = results[i-1][j-1] + 1;
-			}
-			else
-			{
-				results[i][j] = max(results[i][j-1], results[i-1][j]);
-			}
-		}
-	}
-
-	longestSubsequence = trace(results, input, stringLen, stringLen);
-
-	for (int i=0; i<=stringLen; i++)
+	for (int i=0; i<=input.length(); i++)
 	{
 		delete [] results[i];
 	}
@@ -69,16 +46,71 @@ int main()
 	return 0;
 }
 
-string reverseString(string input)
+void printLongestSubsequenceArray(int**& results, string input)
 {
-	string temp;
-
-	for (int i=0; i<input.length(); i++)
+	//This is debug output to look at our results array
+	cout << "  0 ";
+	for (int i=1; i<=input.length(); i++)
 	{
-		temp += input[input.length() - 1 - i];
+		cout << input[input.length() - i] << " ";
+	}
+	cout << endl;
+
+	for (int i=0; i<=input.length(); i++)
+	{
+		if (i == 0)
+		{
+			cout << "0 ";
+		}
+		else
+		{
+			cout << input[i-1] << " ";
+		}
+
+		for (int j=0; j<=input.length(); j++)
+		{
+			cout << results[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void calcResultsRow(int**& results, int row, string input)
+{
+	calcResultsCol(results, row, 0, input);
+
+	if (input.length() > row)
+	{
+		calcResultsRow(results, row + 1, input);
 	}
 
-	return temp;
+	return;
+}
+
+void calcResultsCol(int**& results, int row, int col, string input)
+{
+	if (row == 0 || col == 0)
+	{
+		results[row][col] = 0;
+	}
+	else
+	{
+		if (input[row-1] == input[input.length()-col])
+		{
+			results[row][col] = results[row-1][col-1] + 1;
+		}
+		else
+		{
+			results[row][col] = max(results[row-1][col], results[row][col-1]);
+		}
+	}
+
+	if (input.length() > col)
+	{
+		calcResultsCol(results, row, col+1, input);
+	}
+
+	return;
 }
 
 string trace(int**& results, string input, int row, int col)
